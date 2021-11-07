b1c16a51e9c39617a71adc4927b77adc87f69447
Leslie Kornes <lesliekornes@gmail.com>
2021-11-06 23:49:40 -0400

    demo custom field type example

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,5 +1,5 @@
 class Types::AuthorType < Types::BaseObject
-  description " An author"
+  description "An author"
 
   field :id, ID, null: false
   field :first_name, String, null: true
@@ -7,8 +7,24 @@ class Types::AuthorType < Types::BaseObject
   field :yob, Int, null: false
   field :is_alive, Boolean, null: true
   field :full_name, String, null: true
+  field :coordinates, Types::CoordinatesType, null: false
 
   def full_name
     ([object.first_name, object.last_name].compact).join(" ")
   end
 end
+
+# in Altair:
+# {
+#   author(id: 2) 
+#   {
+#     firstName
+#     isAlive
+#     lastName
+#     fullName
+#     coordinates {
+#       latitude
+#       longitude
+#     }
+#   }
+# }
--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,8 @@ class Author < ApplicationRecord
   # def full_name
   #   ([first_name, last_name].compact).join(" ")
   # end
+
+  def coordinates
+    [rand(90), rand(90)]
+  end
 end
